0dd0a3b59996adc3e22af0b59926d6e2e1bb266b
Yash Handa <yashhanda500@gmail.com>
2025-02-17 20:20:25 +0530

    first commit

---
+++ 01-jpa-one-to-one-uni/src/main/java/com/dilemmawalker/advanced_jpa_mappings/AdvancedJpaMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.dilemmawalker.advanced_jpa_mappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJpaMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedJpaMappingsApplication.class, args);
+	}
+
+}
