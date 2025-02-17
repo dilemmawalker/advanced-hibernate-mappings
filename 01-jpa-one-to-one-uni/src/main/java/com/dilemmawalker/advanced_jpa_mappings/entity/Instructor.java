ac3c54814a6f5c9694e99e0685cc325acf39186d
Yash Handa <yashhanda500@gmail.com>
2025-02-17 23:40:18 +0530

    Added instructor & instructor detail table with foreign key column

---
+++ 01-jpa-one-to-one-uni/src/main/java/com/dilemmawalker/advanced_jpa_mappings/entity/Instructor.java
@@ -0,0 +1,42 @@
+package com.dilemmawalker.advanced_jpa_mappings.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="instructor")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Instructor {
+
+    //annotate class as an entity and map to db table
+
+    //define the fields
+
+//    Setup relationship b/w instructor & instructor detail here
+
+    //annotate the fields with db column names
+
+    //create constructors, getter/setter & toString methods
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="instructor_detail_id")
+    private InstructorDetail instructorDetail;
+}
---
+++ 01-jpa-one-to-one-uni/src/main/java/com/dilemmawalker/advanced_jpa_mappings/entity/InstructorDetail.java
@@ -0,0 +1,33 @@
+package com.dilemmawalker.advanced_jpa_mappings.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="instructor_detail")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class InstructorDetail {
+
+    //annotate class as an entity and map to db table
+
+    //define the fields
+
+    //annotate the fields with db column names
+
+    //create constructors, getter/setter & toString methods
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+}
