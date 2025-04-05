a22996aa0d9034d2e9c5999c1d5fb51c683890be
Yash Handa <yashhanda500@gmail.com>
2025-04-05 16:49:41 +0530

    added student class with mapping to db entities

---
+++ 01-jpa-one-to-one-uni/src/main/java/com/dilemmawalker/advanced_jpa_mappings/entity/Student.java
@@ -0,0 +1,30 @@
+package com.dilemmawalker.advanced_jpa_mappings.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "student")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
